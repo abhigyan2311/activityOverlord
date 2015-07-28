03652ca6699f9962361bf0a9e0ecabb556db3c5c
Abhigyan <abhigyan2311@gmail.com>
2015-07-28 18:38:38 +0530

    User show View Done

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -13,22 +13,30 @@ module.exports = {
   ,
   create: function (req, res, next) {
     User.create(req.params.all(), function userCreated (err, user) {
-
       if(err) {
         console.log(err);
         req.session.flash = {
           err: err
         };
-
         return res.redirect('/user/new');
-
       }
 
-      res.json(user);
-      req.session.flash = {};
+      //res.json(user);
 
-    })
+      res.redirect('/user/show/' + user.id)
+
+    });
+  },
+  show: function(req, res, next) {
+    User.findOne(req.param('id'), function foundUser (err, user) {
+      if(err) return next(err);
+      if(!user) return next();
+      res.view({
+        user: user
+      });
+    });
   }
+
 
 };
 
